7b67843e20aacfb3894214e71408b441a713e0ec
jurba1997 <jurba2011@gmail.com>
2023-05-17 17:59:28 +0000

    add two questions to the javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+/** Questions */
+
+const questions = [{
+    question: "Which of these movies was directed by Quentin Tarantino?",
+    answers: [
+        {text: "The Green Mile", correct:false},
+        {text: "Unbreakable", correct:false},
+        {text: "The Shawshank Redemption", correct:false},
+        {text: "Pulp Fiction", correct:true}
+    ]
+},
+
+    {
+        question: "What are the names of the main characters in the show 'Rick and Morty'?",
+    answers: [
+        {text: "Rick and Morty", correct:true},
+        {text: "Jules and Vince", correct:false},
+        {text: "Brad and Leo", correct:false},
+        {text: "Toby and James", correct:false}
+    ]
+},
+];
